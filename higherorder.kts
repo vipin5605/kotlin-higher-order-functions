86738fb303a2935af9afd049ccbc1dbb35bc7d75
vipin5605 <vipin5605@gmail.com>
2025-09-25 20:15:00 +0400

    Update higherorder.kts

--- higherorder.kts
+++ higherorder.kts
@@ -1,3 +1,5 @@
+// Example 1
+
 fun calculation(x : Int, y: Int, operation : (Int , Int) -> Int) : Int {
 
     return operation(x,y)
@@ -11,3 +13,15 @@ val resultMultiplication = calculation(4, 5, multiplication)
 
 print(resultAddition)
 print(resultMultiplication)
+
+
+// Example 2 - Higher-order functions with receiver (also known as extension function literals with receiver) 
+
+data class Person(val name: String, val age: Int)
+
+fun Person.printDetails() {
+    println("Name: $name, Age: $age")
+}
+
+val person = Person("John Doe", 30)
+person.printDetails() // Output: Name: John Doe, Age: 30
