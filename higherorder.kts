f92dd18c0f9f90ec4bad51b0e65a3b42072fe898
vipin5605 <vipin5605@gmail.com>
2025-09-25 18:52:19 +0400

    Update higherorder.kts

--- higherorder.kts
+++ higherorder.kts
@@ -1,7 +1,13 @@
-fun calculate(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
-    return operation(a, b)
+fun calculation(x : Int, y: Int, operation : (Int , Int) -> Int) : Int {
+
+    return operation(x,y)
 }
 
-val addition = { x: Int, y: Int -> x + y }
-val result = calculate(5, 3, addition) // Result: 8
-print(result)
+val addition = {x : Int, y : Int -> x + y }
+val resultAddition = calculation(4, 5, addition)
+
+val multiplication = {x : Int, y : Int -> x * y }
+val resultMultiplication = calculation(4, 5, multiplication)
+
+print(resultAddition)
+print(resultMultiplication)
